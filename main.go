d7a0f24ce3fbb99383d9218ef12cd48cd872cacd
Johan Gunnarsson <johan.gunnarsson@skymill.io>
2024-04-09 14:00:26 +0200

    ADD chi and gitignore

--- main.go
+++ main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
@@ -12,7 +15,9 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 
 func contactHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	userID := chi.URLParam(r, "userID")
 	fmt.Fprint(w, "<h1>Contact Page</h1> <p>To get in touch, email me at <a href=\"mailto:contact@gamil.com\">contact@gmail.com</a>.")
+	fmt.Fprint(w, "<h2>User ID received: ", userID)
 }
 
 func faqHandler(w http.ResponseWriter, r *http.Request) {
@@ -28,33 +33,16 @@ func faqHandler(w http.ResponseWriter, r *http.Request) {
 	</ul>`)
 }
 
-func pathHandler(w http.ResponseWriter, r *http.Request) {
-	switch r.URL.Path {
-	case "/":
-		homeHandler(w, r)
-	case "/contact":
-		contactHandler(w, r)
-	case "/faq":
-		faqHandler(w, r)
-	default:
-		http.Error(w, "whoops, 404 it is, that's no gouda, try another path ", http.StatusNotFound)
-	}
-}
-
-// type Router struct{}
-
-// func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-// 	switch r.URL.Path {
-// 	case "/":
-// 		homeHandler(w, r)
-// 	case "/contact":
-// 		contactHandler(w, r)
-// 	default:
-// 		http.Error(w, "whoops, 404 it is, that's no gouda, try another path ", http.StatusNotFound)
-// 	}
-// }
-
 func main() {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/", homeHandler)
+	r.Get("/contact/{userID}", contactHandler)
+	r.Get("/faq", faqHandler)
+	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "whoops, 404 it is, that's no gouda, try another path ", http.StatusNotFound)
+	})
+
 	fmt.Println("Starting server on : 3000...")
-	http.ListenAndServe(":3000", http.HandlerFunc(pathHandler))
+	http.ListenAndServe(":3000", r)
 }
