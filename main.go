11496ad68489607204736a7050e67fd47010d1a7
Johan Gunnarsson <johan-gunnarsson@hotmail.com>
2024-05-31 09:08:36 +0200

    ADD signup page and endpoint

--- main.go
+++ main.go
@@ -28,6 +28,11 @@ func main() {
 		"faq.gohtml", "tailwind.gohtml",
 	))))
 
+	r.Get("/signup", controllers.StaticHandler(views.Must(views.ParseFS(
+		templates.FS,
+		"signup.gohtml", "tailwind.gohtml",
+	))))
+
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "whoops, 404 it is, that's no gouda, try another path ", http.StatusNotFound)
 	})
