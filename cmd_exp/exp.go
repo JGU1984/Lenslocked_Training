b30696eeab4378b1e5f2aef74b16ffc847fbee7c
Johan Gunnarsson <johan-gunnarsson@hotmail.com>
2024-06-25 15:12:20 +0200

    ADD some sample database interactions

--- cmd_exp/exp.go
+++ cmd_exp/exp.go
@@ -41,13 +41,16 @@ func main() {
 	}
 	fmt.Println("Connected!")
 
+	//########################################################################################################################################
+
+	// creating and setting up the database tables
 	_, err = db.Exec(`
 		CREATE TABLE IF NOT EXISTS users (
 			id SERIAL PRIMARY KEY,
 			name TEXT,
 			email TEXT UNIQUE NOT NULL
 		);
-		
+
 		CREATE TABLE IF NOT EXISTS orders (
 			id SERIAL PRIMARY KEY,
 			user_id INT NOT NULL,
@@ -59,4 +62,95 @@ func main() {
 		panic(err)
 	}
 	fmt.Println("Tables created.")
+
+	//########################################################################################################################################
+
+	// inserting vaules to the tables $1, $2 is to insert the variables data, this to avoid sql injection
+	// returning id, let's us retrieve the id generated when doing the insert queries
+
+	// name := "Test"
+	// email := "test@example2.com"
+	// row := db.QueryRow(`
+	// 	INSERT INTO users (name, email)
+	// 	VALUES ($1, $2) RETURNING id;`, name, email)
+	// var id int
+	// err = row.Scan(&id)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// fmt.Println("user created. id=", id)
+
+	//########################################################################################################################################
+
+	// Querying single record
+
+	// id := 1
+	// row := db.QueryRow(`
+	// 	SELECT name, email
+	// 	FROM users
+	// 	WHERE id=$1;`, id)
+	// var name, email string
+	// err = row.Scan(&name, &email)
+	// if err == sql.ErrNoRows {
+	// 	fmt.Println("Error, no rows!")
+	// }
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// fmt.Printf("User information: name=%s email=%s\n", name, email)
+
+	//########################################################################################################################################
+
+	// Creating some fake orders
+
+	// userID := 1
+	// for i := 1; i <= 5; i++ {
+	// 	amount := i * 100
+	// 	desc := fmt.Sprintf("Fake order #%d", i)
+	// 	_, err := db.Exec(`
+	// 		INSERT INTO orders(user_id, amount, description)
+	// 		VALUES($1, $2, $3)`, userID, amount, desc)
+	// 	if err != nil {
+	// 		panic(err)
+	// 	}
+	// }
+	// fmt.Println("Created fake orders.")
+
+	//########################################################################################################################################
+
+	// Querying multi records
+
+	type Order struct {
+		ID          int
+		UserID      int
+		Amount      int
+		Description string
+	}
+
+	var orders []Order
+
+	userID := 1
+	rows, err := db.Query(`
+	SELECT id, amount, description
+	FROM orders
+	WHERE user_id=$1`, userID)
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var order Order
+		order.UserID = userID
+		err := rows.Scan(&order.ID, &order.Amount, &order.Description)
+		if err != nil {
+			panic(err)
+		}
+		err = rows.Err()
+		if err != nil {
+			panic(err)
+		}
+		orders = append(orders, order)
+	}
+	fmt.Println("Orders:", orders)
 }
